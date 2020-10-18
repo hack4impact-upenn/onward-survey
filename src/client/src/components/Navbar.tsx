5d8e392bdd60660119fbd2f13919991e5d1d6e7f
Grace Jiang <gracjiang@gmail.com>
2020-10-17 22:06:17 -0700

    buggy navbar – fix later

--- src/client/src/components/Navbar.tsx
+++ src/client/src/components/Navbar.tsx
@@ -4,14 +4,22 @@ import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import auth from '../api/core/auth';
 
+// image imports
+import logo from "../assets/logo-full.png";
+
+// style imports
+import '../styles/navbar.css';
+
+
 const NavbarContainer = styled.div`
   width: 100vw;
-  height: 50px;
+  height: 60px;
   padding: 0px 50px;
   display: flex;
   align-items: center;
-  background-color: #2c3e50;
+  background-color: #ffffff;
   justify-content: space-between;
+  box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.15);
 `;
 
 const NavbarItem = styled.div`
@@ -19,6 +27,49 @@ const NavbarItem = styled.div`
     cursor: pointer;
   }
 `;
+
+const Logo = styled.div`
+  width: 120px;
+  margin-top: 2px;
+`;
+
+
+class ProfileMenu extends React.Component<{}, {open: boolean}> {
+
+  state = {
+    open: false,
+  };
+
+  toggleMenu = () => {
+    this.setState(state => {
+      return {
+        open: !state.open,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="navbar-container">
+        <button type="button" className="button" onClick={this.toggleMenu}>
+          Profile
+        </button>
+        {this.state.open && (
+          <div className="dropdown">
+            <ul className="navbar-ul">
+              <li className="navbar-li">
+                Profile
+              </li>
+              <li className="navbar-li" onClick={() => auth.logout()}>
+                Logout
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 const Navbar = () => {
   let history = useHistory();
@@ -39,20 +90,30 @@ const Navbar = () => {
   return (
     <NavbarContainer>
       <NavbarItem>
-        <a href="/" className="title is-6 has-text-white">
-          Onward Financial
+        <a href="/" className="title is-6">
+          <Logo>
+            <img src={logo} alt="Onward Financial"></img>
+          </Logo>
         </a>
       </NavbarItem>
       <NavbarItem>
+        
+      </NavbarItem>
+      <NavbarItem>
+        {loggedIn && (
+          <ProfileMenu></ProfileMenu>
+        )}
+      </NavbarItem>
+      {/* <NavbarItem>
         {loggedIn && (
           <h1
-            className="title is-6 has-text-white"
+            className="title is-6"
             onClick={() => auth.logout()}
           >
             Logout
           </h1>
         )}
-      </NavbarItem>
+      </NavbarItem> */}
     </NavbarContainer>
   );
 };
