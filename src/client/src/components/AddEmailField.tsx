c8181946ade662af44a96bf8dcd2f7a8ef37b45b
Eric Chen <eric.chen.us@gmail.com>
2020-10-28 16:40:35 -0500

    email button

---
+++ src/client/src/components/AddEmailField.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import auth from '../api/core/auth';
+
+interface Props {}
+const Table: React.FC<Props> = (props) => {
+    return(
+        <form>
+            <label>
+                <input type="text" name="email" />
+            </label>
+            <input type="add_email" value="Add Email" />
+        </form>
+    )
+}
+
+export default Table;
+   
