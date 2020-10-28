4848e3dec0588edded4f656223ddc704ccc13241
Ivan Esmeral <ivanesmeral@Ivans-MBP.fios-router.home>
2020-10-28 17:49:47 -0400

    survey page and component for textbox

--- src/client/src/pages/AppRouter.tsx
+++ src/client/src/pages/AppRouter.tsx
@@ -4,9 +4,11 @@ import Signup from './Signup';
 import Login from './Login';
 import Dashboard from './Dashboard';
 import Profile from './Profile';
+import SurveyQuestions from './SurveyQuestions';
 import AppContainer from '../components/AppContainer';
 import PrivateRoute from '../components/PrivateRoute';
 import PublicRoute from '../components/PublicRoute';
+
 
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
@@ -20,7 +22,9 @@ const AppRouter = () => {
           <PublicRoute exact path="/login" component={Login} />
           <PrivateRoute exact path="/dashboard" component={Dashboard} />
           <PrivateRoute exact path="/profile" component={Profile} />
+          <PublicRoute exact path="/survey/questions" component={SurveyQuestions} />
           <PublicRoute exact={false} path="/" component={Main} />
+          
         </Switch>
       </AppContainer>
     </Router>
