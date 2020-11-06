6fdae89a77555b1853abf5ec4d3ad56f2a8baa07
Silvi Kabra <silvi.kabra@gmail.com>
2020-11-06 00:12:49 -0500

    Survey data posted to DB and completed field gets updated

--- src/routes/employee.api.ts
+++ src/routes/employee.api.ts
@@ -22,19 +22,33 @@ router.get('/completed', (req, res) => {
 // submit survey response
 // change employee completed survey to true
 router.post('/survey', async (req, res) => {
-  const { surveyId, responses } = req.body;
+  const { employeeId, responses } = req.body;
+  const employee = Employee.findById(employeeId)
+    .select("surveyId")
+    .then(async (employee) => {
+      if (!employee) return errorHandler(res, 'Employee does not exist.');
 
-  const newEmployeeResponse = new EmployeeResponse({
-    surveyId,
-    responses,
-  });
+      const surveyId = employee.surveyId;
+      const newEmployeeResponse = new EmployeeResponse({
+        surveyId,
+        responses,
+      });
 
-  // update employee completed survey to true
-
-  return newEmployeeResponse
-    .save()
-    .then(() => res.status(200).json({ success: true }))
-    .catch((e) => errorHandler(res, e.message));
+      // update employee completed survey to true
+      try {
+        await newEmployeeResponse.save();
+        await Employee.updateOne(
+          {_id: employeeId},
+          { $set: { completed: true } }
+        )
+      }
+      catch (err) {
+        console.log(err);
+        return errorHandler(res, err);
+      }
+      return res.status(200).json({ message: 'success' });
+    })
+    .catch((err) => errorHandler(res, err.message));
 });
 
 //redirect to survey
@@ -54,13 +68,28 @@ router.route('/survey/:employerSurveyId-:surveyId').get((req, res) => {
       if (!employee) return errorHandler(res, 'Employee does not exist.');
       //for some reason employee.completed is always true regardless of its value
       if (employee.completed){
-       res.render('../client/src/pages/survey/PLACEHOLDER'); //need a view engine to render
+       res.render('../client/src/pages/survey/expired'); //need a view engine to render
        return res.status(200).json({ success: true, message: 'Employee has already opened survey' });
       }
-    res.render('../client/src/pages/survey/welcome');
+    res.render('../client/src/pages/survey/'+surveyUrl+'/'+surveyId+'/welcome');
     return res.status(200).json({ success: true, message: 'Employee opened survey' });
   })
   .catch((err) => errorHandler(res, err.message));
+});
+
+// get me
+// protected route (should be protected is not atm)
+router.get('/me/surveyId', (req, res) => {
+  const { employeeId }  = req.body;
+
+  return Employee.findById(employeeId)
+    .select('surveyId')
+    .then((employee) => {
+      if (!employee) return errorHandler(res, 'Employee does not exist.');
+
+      return res.status(200).json({ success: true, data: employee });
+    })
+    .catch((err) => errorHandler(res, err.message));
 });
 
 export default router;
