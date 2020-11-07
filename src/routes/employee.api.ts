1efa5ffaba9d6deb990a58d6717cab9804c8038b
Silvi Kabra <silvi.kabra@gmail.com>
2020-11-06 22:16:17 -0500

    Routing to survey welcome page success

--- src/routes/employee.api.ts
+++ src/routes/employee.api.ts
@@ -8,24 +8,15 @@ const router = express.Router();
 
 // get whether the employee has completed the survey
 // should be simple
-router.get('/completed', (req, res) => {
-  const { surveyId } = req.body;
+router.get('/:surveyId/completed', (req, res) => {
+  const surveyId  = req.params.surveyId;
   return Employee.findOne( {"surveyId": surveyId})
     .select("_id employer completed")
     .then((employee) => {
-      if (!employee) return errorHandler(res, 'Employee does not exist.');
+      if (!employee) return errorHandler(res, 'Employee with survey ID ' + req.body.surveyId + ' does not exist.');
       return res.status(200).json({ success: true, data: employee });
     })
-    .catch((err) => errorHandler(res, err.message));
-
-  /*const { _id }  = req.body;
-  return Employee.findById(_id)
-    .select("firstName lastName completed _id")
-    .then((employee) => {
-      if (!employee) return errorHandler(res, 'Employee does not exist.');
-      return res.status(200).json({ success: true, data: employee });
-    })
-    .catch((err) => errorHandler(res, err.message));*/
+    .catch((err) =>  errorHandler(res, err.message));
 });
 
 // submit survey response
@@ -61,7 +52,7 @@ router.post('/survey', async (req, res) => {
 });
 
 //redirect to survey
-router.route('/survey/:employerSurveyId-:surveyId').get((req, res) => {
+/*router.route('/survey/:employerSurveyId-:surveyId').get((req, res) => {
   const surveyId = req.params.surveyId;
   const surveyUrl = req.params.employerSurveyId
   const user = User.findOne({ surveyUrl })
@@ -83,10 +74,9 @@ router.route('/survey/:employerSurveyId-:surveyId').get((req, res) => {
     return res.status(200).json({ success: true, message: 'Employee opened survey' });
   })
   .catch((err) => errorHandler(res, err.message));
-});
+});*/
 
-// get me
-// protected route (should be protected is not atm)
+// get surveyId from user
 router.get('/me/surveyId', (req, res) => {
   const { employeeId }  = req.body;
 
