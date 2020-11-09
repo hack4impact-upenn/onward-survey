ee7de97d582ffb8bc0fb1e48cdc86e0465c00b0e
Abhishek Pandya <abhishekp213@hotmail.com>
2020-11-08 22:32:05 -0500

    form keeps track of email text input

--- src/client/src/components/AddEmailField.tsx
+++ src/client/src/components/AddEmailField.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import auth from '../api/core/auth';
+import { TextInput } from 'react-native';
 
 const AddEmailGroup = styled.div`
     padding-bottom: 34px;
@@ -10,20 +11,44 @@ const AddEmailGroup = styled.div`
 `;
 
 interface Props {}
-const Table: React.FC<Props> = (props) => {
-    return(
-        <AddEmailGroup className="field is-grouped">
-            <p className="control is-expanded">
-            <input className="input" type="text" placeholder="Enter In Email"/>
-            </p>
-            <p className="control">
-                <a className="button is-info">
-                    Add Email
-                </a>
-            </p>
-        </AddEmailGroup>
-    )
+class AddEmail extends React.Component<{}, {emailInput: string}> {
+    constructor(props:any) {
+        super(props);
+        this.state = {emailInput : ''};
+    }
+
+    handleChange(e : any) {
+        this.setState({emailInput: e.target.value})
+    }
+
+    handleClick() {
+        // make post request
+        alert('Your input value is ' + this.state.emailInput);
+    }
+
+    render() {
+        return(
+            <AddEmailGroup className="field is-grouped">
+                <p className="control is-expanded">
+                <input 
+                    className="input" 
+                    type="text" 
+                    placeholder="Enter In Email"
+                    onChange={(e) => this.handleChange(e)}
+                />
+                </p>
+                <p className="control">
+                    <a 
+                        className="button is-info"
+                        onClick={() => this.handleClick()}
+                    >
+                        Add Email
+                    </a>
+                </p>
+            </AddEmailGroup>
+        );
+    }
 }
 
-export default Table;
+export default AddEmail;
    
