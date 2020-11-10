98936b4dfc3bc33aa6a25e4505e3957fc7358f21
Daniel Tian <dtian.95@gmail.com>
2020-11-10 13:07:32 +0800

    format

--- src/client/src/pages/ManageSurvey.tsx
+++ src/client/src/pages/ManageSurvey.tsx
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import '../styles/manage_survey.css';
-import SurveyTabs from "../components/SurveyTabs";
+import SurveyTabs from '../components/SurveyTabs';
 
 interface Props {}
 const ManageSurvey: React.FC<Props> = (props) => {
-  return (
-    <SurveyTabs></SurveyTabs>
-  );
+  return <SurveyTabs></SurveyTabs>;
 };
 
 export default ManageSurvey;
