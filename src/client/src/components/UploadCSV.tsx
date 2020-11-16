6eff7704c49bd528e38ddc01dc5c54c8a69d955a
Abhishek Pandya <abhishekp213@hotmail.com>
2020-11-16 14:14:27 -0500

    bad commit

--- src/client/src/components/UploadCSV.tsx
+++ src/client/src/components/UploadCSV.tsx
@@ -1,30 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SurveyButtonGroup = styled.form`
+const SurveyButtonGroup = styled.form``;
 
-`;
+const InputWrapper = styled.div``;
 
-const InputWrapper = styled.div`
-
-`;
-
-interface Props {};
+interface Props {}
 const UploadCSV: React.FC<Props> = (props) => {
-    return (
-        <InputWrapper className="columns">
-            <p className="control is-expanded">
-                <input
-                className="input"
-                />
-            </p>
-            
-            <SurveyButtonGroup className="column is-two-fifths">
-                <button className="button is-primary ">Choose File</button>
-                <button className="button is-primary ">Upload</button>
-            </SurveyButtonGroup>
-        </InputWrapper>
-    ); 
-}
+  return (
+    
+  );
+};
 
 export default UploadCSV;
