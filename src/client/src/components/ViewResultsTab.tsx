d46cc730f68449ba213c86c3a169865b0a27be7f
Eric Chen <eric.chen.us@gmail.com>
2020-11-18 00:18:00 -0600

    moved data into hashmaps for each q

--- src/client/src/components/ViewResultsTab.tsx
+++ src/client/src/components/ViewResultsTab.tsx
@@ -15,6 +15,59 @@ const employeeQuery = useQuery(
   }
 );
 
+const survey : String[][] = [[],[],[],[],[],[]]
+const counters : Map<String,number>[] = []
+
+const organizeArray = (results:any) => {
+  for (var i = 0; i < results.length; i++)
+  {
+    
+    const result = results[i];
+
+    if (result.responses instanceof Array)
+    {
+      for (var j = 0; j < result.responses.length; j++)
+      {
+        const answerObject = result.responses[j]
+        survey[j].push(answerObject[Object.keys(answerObject)[0]]);
+      }
+    }
+  }
+}
+//
+//
+// assuming that there is one answer per question (not an array)
+//
+//
+
+const partitionIntoMaps = () =>
+{
+  for (var i = 0; i < survey.length; i++)
+  {
+    const numberedArray = survey[i];
+    for (var j = 0; j < survey.length; j++)
+    {
+      
+        let prevCount = counters[i].get(numberedArray[j])
+        if (!prevCount)
+        {
+          prevCount = 0
+        }
+        counters[i].set(numberedArray[j], prevCount+1)
+    }
+  }
+}
+
+const mapsToArrays = () =>
+{
+  
+}
+
+
+
+
+
+
 const ViewResultsPage: React.FC<Props> = (props) => {
   return (
     <div>
