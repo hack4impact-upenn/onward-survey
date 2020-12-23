aeb70d7ac90d9b2daa2c872a02278002700106c8
Daniel Tian <dtian.95@gmail.com>
2020-12-23 03:04:13 -0500

    updated test

--- src/client/src/tests/App.test.js
+++ src/client/src/tests/App.test.js
@@ -6,10 +6,8 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 // import component or page units
-import Main from '../pages/Main';
 import Dashboard from '../pages/Dashboard';
 
 it('renders without crashing', () => {
-  shallow(<Main />);
   shallow(<Dashboard />);
 });
